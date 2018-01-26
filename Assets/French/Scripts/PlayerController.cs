fe0c9996630ea300e0051ee7726aaef6d44d3029
Andrea Franciosi <french_04@hotmail.it>
2018-01-26 23:59:16 +0100

    prima scena base

---
+++ Assets/French/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController control;
+
+    float v;
+    float h;
+    [SerializeField] float gravity;
+    float vSpeed;
+    [SerializeField] float maxVSpeed;
+
+    Vector3 MoveVector;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        control = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(-v, 0, h);
+	}
+
+    void Gravity()
+    {
+        if(!control.isGrounded)
+        {
+            vSpeed -= gravity;
+            vSpeed = Mathf.Min(vSpeed, maxVSpeed);
+        }
+    }
+}
