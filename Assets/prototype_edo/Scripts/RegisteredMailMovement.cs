cca6cd2e2c2a9373197eb9ff49d0ad0fd73b010a
Edoardo Giacomello <edoardo.giacomello1990@gmail.com>
2018-01-27 11:59:59 +0100

    RegisteredMail (Raccomandata) Movement
    TEST ME :)

---
+++ Assets/prototype_edo/Scripts/RegisteredMailMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisteredMailMovement : MonoBehaviour {
+
+
+    Rigidbody rig;
+    float t;
+    public float amplitude = 15f;
+    public float frequency = 15f;
+    // Use this for initialization
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+
+    }
+
+    private void OnEnable()
+    {
+        t = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        t += Time.deltaTime;
+        var locVel = transform.InverseTransformDirection(rig.velocity);
+        locVel.x = amplitude * Mathf.Sin(frequency * t);
+        locVel.z = amplitude * Mathf.Cos(frequency * t);
+        rig.velocity = transform.TransformDirection(locVel);
+    }
+}
