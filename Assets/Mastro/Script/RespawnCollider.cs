79949d2374716930838f35935ea7f44799f41586
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2018-01-27 16:26:44 +0100

    RespawnPickUp

---
+++ Assets/Mastro/Script/RespawnCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnCollider : MonoBehaviour {
+
+    public RespawnPickUp respawn;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            respawn.TakeIt = true;
+        }
+    }
+}
