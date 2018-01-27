a9cd81dead471bbb57e395beae0c147a0d5072da
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2018-01-27 20:28:32 +0100

    modifica PickUp + Inserimento respawn

--- Assets/Mastro/Script/PickUp.cs
+++ Assets/Mastro/Script/PickUp.cs
@@ -5,16 +5,56 @@ using UnityEngine;
 public class PickUp : MonoBehaviour {
 
 	PlayerController player;
+    InputController control;
     public GameObject bullet;
     public BulletForce.BulletType bulletType;
 	public Texture2D bulletIcon;
+    MeshRenderer myRender;
 
-	void OnTriggerEnter(Collider hit)
+    public float enabledTime;
+    float takeItMoment;
+    float startAnimation;
+
+    bool isPickUppable = true;
+
+    void Start()
+    {
+        myRender = GetComponent<MeshRenderer>();
+        startAnimation = enabledTime - 1f;
+    }
+
+    private void Update()
+    {
+        if (enabledTime <= Time.time - takeItMoment && myRender.enabled == false)
+        {            
+            myRender.enabled = true;
+            isPickUppable = true;
+        }
+        if (startAnimation <= Time.time - takeItMoment && myRender.enabled == false)
+        {
+            
+            
+        }
+        
+    }
+    
+    void OnTriggerStay(Collider hit)
 	{
 		if (hit.CompareTag("Player"))
 		{
-			player = hit.GetComponent<PlayerController>();
-			player.SetShouldCarry(true, bullet, gameObject, bulletType, bulletIcon);
+            player = hit.GetComponent<PlayerController>();
+            control = hit.GetComponent<InputController>();
+
+            if(isPickUppable)
+			    player.SetShouldCarry(true, bullet, gameObject, bulletType, bulletIcon);
+
+            if (player.carrying == true && control.isFiring())
+            {
+                myRender.enabled = false;
+                isPickUppable = false;
+                takeItMoment = Time.time;
+            }
+
 		}
 	}
 
@@ -25,4 +65,7 @@ public class PickUp : MonoBehaviour {
 			player.SetShouldCarry(false, null, gameObject, bulletType, bulletIcon);
 		}
 	}
+
+
+
 }
