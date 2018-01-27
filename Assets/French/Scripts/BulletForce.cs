8507cbe8079f5580dcdd0d372a89a436817fd40c
Andrea Franciosi <french_04@hotmail.it>
2018-01-27 02:09:01 +0100

    prefab proiettile + sparo nella direzione

---
+++ Assets/French/Scripts/BulletForce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletForce : MonoBehaviour
+{
+    PlayerController player;
+    Vector3 direction;
+    public int speed = 2;
+
+    private void OnEnable()
+    {
+        player = GameObject.FindObjectOfType<PlayerController>();
+        direction = player.lastDirection;
+    }
+
+    private void Update()
+    {
+        transform.position += direction * Time.deltaTime * speed; 
+    }
+}
