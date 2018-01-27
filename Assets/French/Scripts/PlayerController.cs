a5edf75c5628b65921d960f4675f237e39395c19
MirtoRande <mirtorande@gmail.com>
2018-01-27 01:19:05 +0100

    roll

--- Assets/French/Scripts/PlayerController.cs
+++ Assets/French/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     CharacterController control;
+	SpriteRenderer renderer;
 
     float v;
     float h;
@@ -13,18 +14,32 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float maxVSpeed;
 
     Vector3 moveVector;
+	Vector3 lastDirection;
 	[SerializeField] float moveSpeed;
 
+	bool rolling = false;
+	[SerializeField] float rollSpeed;
+	[SerializeField] float rollTime;
+	float rollTimer;
 
     void Start ()
     {
         control = GetComponent<CharacterController>();
+		renderer = GetComponent<SpriteRenderer>();
+
+		lastDirection = new Vector3(-1,0,0);
 	}
 	
 	void Update ()
     {
-        v = Input.GetAxis("Vertical");
-        h = Input.GetAxis("Horizontal");
+        v = -Input.GetAxis("Vertical");
+        h = -Input.GetAxis("Horizontal");
+
+		if (Input.GetAxis("Jump") > 0)
+		{
+			rolling = true;
+			rollTimer = rollTime;
+		}
 
 		ManageMovement();
 	}
@@ -34,7 +49,20 @@ public class PlayerController : MonoBehaviour
 	{
 		moveVector = new Vector3(h, 0, v);
 		moveVector.Normalize();
-		moveVector *= moveSpeed;
+
+		if(moveVector.magnitude > 0)
+			lastDirection = moveVector;
+
+		if (!rolling)
+			moveVector *= moveSpeed;
+		else
+		{
+			moveVector = lastDirection * rollSpeed;
+			if (rollTimer > 0)
+				rollTimer -= Time.deltaTime;
+			else
+				rolling = false;
+		}
 
 		Gravity();
 
