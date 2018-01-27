21fab8f1178a0a005cb38b7b3c9d0e5cfbd8152b
Edoardo Giacomello <edoardo.giacomello1990@gmail.com>
2018-01-27 02:03:52 +0100

    InputController
    Code for managing xbox joypads

---
+++ Assets/prototype_edo/Scripts/InputController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class InputController : MonoBehaviour {
+
+    private bool hasToFire = false;
+    public PlayerIndex playerControllerIndex;
+    private GamePadState state;
+    
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+
+   
+    //Dash
+    //Prendi, lancia, parata
+    public Vector3 getDirection() {
+        float horizontal = state.ThumbSticks.Left.X;
+        float vertical = state.ThumbSticks.Left.Y;
+        return new Vector3(horizontal, vertical, 0.0f);
+    }
+
+    public bool isFiring() {
+        state = GamePad.GetState(playerControllerIndex);
+        return state.Buttons.A.Equals(ButtonState.Pressed);
+     
+    }
+
+    public bool isDashing()
+    {
+        state = GamePad.GetState(playerControllerIndex);
+        return state.Buttons.X.Equals(ButtonState.Pressed);
+
+    }
+
+
+}
