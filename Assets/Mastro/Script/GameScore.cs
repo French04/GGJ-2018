1f8ee47de623ffd652a5243b2d5571d428bef0f6
MirtoRande <mirtorande@gmail.com>
2018-01-28 08:54:59 +0100

    intro

--- Assets/Mastro/Script/GameScore.cs
+++ Assets/Mastro/Script/GameScore.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine.UI;
 
 public class GameScore : MonoBehaviour {
 
@@ -13,6 +13,9 @@ public class GameScore : MonoBehaviour {
 	[SerializeField] GameObject scoreboard1;
 	[SerializeField] GameObject scoreboard2;
 	[SerializeField] GameObject timer;
+	[SerializeField] Text uiText;
+	[SerializeField] float interval;
+	int textStage;
 
 	TextMesh scoreText1;
 	TextMesh scoreText2;
@@ -24,6 +27,32 @@ public class GameScore : MonoBehaviour {
 		scoreText2 = scoreboard2.GetComponent<TextMesh>();
 		timerText = timer.GetComponent<TextMesh>();
 		UpdateScore(0);
+		StartCoroutine(StartGame());
+	}
+
+
+	IEnumerator StartGame()
+	{
+		while (textStage < 4)
+		{
+			yield return new WaitForSeconds(interval);
+			switch (textStage)
+			{
+				case 0:
+					uiText.text = "Ready?";
+					break;
+				case 1:
+					uiText.text = "Set";
+					break;
+				case 2:
+					uiText.text = "Go!";
+					break;
+				case 3:
+					uiText.text = "";
+					break;
+			}
+			textStage++;
+		}
 	}
 
 
