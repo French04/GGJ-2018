0ea93db996955a3b9489035447c1d870045825d2
MirtoRande <mirtorande@gmail.com>
2018-01-28 04:11:14 +0100

    hitsparks

---
+++ Assets/sparkDespawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sparkDespawn : MonoBehaviour {
+
+	[SerializeField] float time;
+
+
+	void Update ()
+	{
+		if (time > 0)
+		{
+			time -= Time.deltaTime;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+}
